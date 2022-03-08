a05a55fcda6002584c6dfa76ac477349dfa91237
Syihab2708 <100379083+Syihab2708@users.noreply.github.com>
2022-03-09 02:27:47 +0700

    Update__main__.py
    hmm

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text="About VylanXRobot", callback_data="emiko_"),
+        InlineKeyboardButton(text="AboutVylanXRobot", callback_data="Vylan_"),
     ],
     [
         InlineKeyboardButton(text="Get Help", callback_data="help_back"),
